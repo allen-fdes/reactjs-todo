ad0b0f166ed0227383f8c368c6b64148cff4c9d5
ln <allen.fdes@gmail.com>
2017-10-10 21:27:26 +0530

    render hello & set background css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var destination = document.querySelector("#root")
+ReactDom.render(<div>
+    <p>Hello!</p>
+</div>,destination);
