ee0df59a8b5d61b08aacfd13c3b5d437b24d7c02
ln <allen.fdes@gmail.com>
2017-10-11 17:57:28 +0530

    add task to state

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            items: []
+        };    
+
+        this.addItem = this.addItem.bind(this);
+    }
+    
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+            
+        
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value =  "";
+            console.log(itemArray);
+            e.preventDefault();
+        }
+
+
+    }
+
     render() {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input type="text" placeholder="Enter Task"/>
+                    <form onSubmit={this.addItem}>
+                        <input type="text" ref = {(a) => this._inputElement = a } placeholder="Enter Task"/>
                         <button type="submit">Add</button>
                     </form>
                 </div>
