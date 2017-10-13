050e85ea7757593a6aade147a1da0706015ec9a2
ln <allen.fdes@gmail.com>
2017-10-13 23:36:47 +0530

    delete code

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -5,10 +5,15 @@ import React, { Component } from 'react';
     constructor(props, context) {
         super(props, context);
         this.createTasks = this.createTasks.bind(this);
+        this.delete = this.delete.bind(this);
     }
 
     createTasks(item) {
-        return <li key={item.key}> {item.text} </li>
+        return <li onClick={ (e) => this.delete(item.key, e)} key={item.key}> {item.text} </li>
+    }
+
+    delete(key) {
+        this.props.delete(key);
     }
 
     render(){
